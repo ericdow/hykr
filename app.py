c460032eb21c5c2170a7beccbda6493058a96661
Eric <ericdow@alum.mit.edu>
2021-07-20 18:04:47 -0400

    started adding bing map data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, jsonify
-import os, sys, math
+import os, sys
 from elevation_data import *
 
 app = Flask(__name__)
@@ -14,42 +14,18 @@ def calculate_result():
     elev_source = request.args.get('elev_source')
     algo = request.args.get('algo')
 
-    # form a bounding box of points to evaluate the elevations at
-    nx = 30
-    ny = 30
-    lat_mid = 0.5*(lat_start + lat_end)
-    lat_dist = math.fabs(lat_end - lat_start)*111045.0
-    long_mid = 0.5*(long_start + long_end)
-    Re = 6.371e6 # Earth radius in meters
-    r_at_lat = math.cos(math.radians(math.fabs(lat_mid)))*Re # radius at lat_mid
-    long_dist = r_at_lat*math.radians(math.fabs(long_end-long_start))
-    
-    d = math.sqrt(lat_dist**2 + long_dist**2)
-    mult = 1.2 # multiplier for expanding bounding box
-    
-    lat_min = lat_mid - 0.5*mult*d/Re*180.0/math.pi
-    lat_max = lat_mid + 0.5*mult*d/Re*180.0/math.pi
-    dlat = (lat_max - lat_min) / ny
-    lat_dist = math.fabs(lat_max - lat_min)*111045.0
-    
-    long_min = long_mid - 0.5*mult*d/r_at_lat*180.0/math.pi
-    long_max = long_mid + 0.5*mult*d/r_at_lat*180.0/math.pi
-    dlong = (long_max - long_min) / nx
-    long_dist = r_at_lat*math.radians(math.fabs(long_max-long_min))
-    
-    lat_long_to_eval = []
-    for i in range(nx):
-        long = long_min + i*dlong
-        for j in range(ny):
-            lat = lat_min + j*dlat
-            lat_long_to_eval.append((lat,long))
-
     # compute the elevation data
+    nx = 32
+    ny = 32
+    d_mult = 1.2
     if elev_source == 'open_topo_data':
         elev_server = OpenTopoData()
     elif elev_source == 'epqs':
         elev_server = EPQSData()
-    elev = elev_server.get_elevations(lat_long_to_eval)
+    elif elev_source == 'bing_maps':
+        elev_server = BingMapData()
+    elev = elev_server.get_elevations(lat_start, long_start, lat_end, long_end, 
+            nx, ny, d_mult)
 
     return jsonify({"elev":elev, "nx":nx, "ny":ny, "lat_dist":lat_dist, 
         "long_dist":long_dist})
--- elevation_data.py
+++ elevation_data.py
@@ -1,6 +1,4 @@
-import urllib.request
-import json
-import time
+import urllib.request, json, time, math
 
 class ElevationData:
     '''Base class for elevation data objects, which are responsible for 
@@ -12,9 +10,40 @@ class ElevationData:
         '''Check if the data server is healthy and return a bool'''
         raise NotImplementedError('You need to define is_healthy()!')
 
-    def get_elevations(self, lat_lng_list):
+    def get_elevations(self, lat_start, long_start, lat_end, long_end, nx, ny, 
+            d_mult):
         '''Get the elevation at a given latitudes and longitudes'''
         raise NotImplementedError('You need to define get_elevation()!')
+
+    def get_lat_long_grid(lat_start, long_start, lat_end, long_end, nx, ny, 
+            d_mult):
+        # form a bounding box of points to evaluate the elevations at
+        lat_mid = 0.5*(lat_start + lat_end)
+        lat_dist = math.fabs(lat_end - lat_start)*111045.0
+        long_mid = 0.5*(long_start + long_end)
+        Re = 6.371e6 # Earth radius in meters
+        r_at_lat = math.cos(math.radians(math.fabs(lat_mid)))*Re # radius at lat_mid
+        long_dist = r_at_lat*math.radians(math.fabs(long_end-long_start))
+        
+        d = math.sqrt(lat_dist**2 + long_dist**2)
+        lat_min = lat_mid - 0.5*d_mult*d/Re*180.0/math.pi
+        lat_max = lat_mid + 0.5*d_mult*d/Re*180.0/math.pi
+        dlat = (lat_max - lat_min) / ny
+        lat_dist = math.fabs(lat_max - lat_min)*111045.0
+        
+        long_min = long_mid - 0.5*d_mult*d/r_at_lat*180.0/math.pi
+        long_max = long_mid + 0.5*d_mult*d/r_at_lat*180.0/math.pi
+        dlong = (long_max - long_min) / nx
+        long_dist = r_at_lat*math.radians(math.fabs(long_max-long_min))
+        
+        lat_long_list = []
+        for i in range(nx):
+            long = long_min + i*dlong
+            for j in range(ny):
+                lat = lat_min + j*dlat
+                lat_long_list.append((lat,long))
+
+        return lat_long_list
 
 class OpenTopoData(ElevationData):
     '''Elevation data from opentopodata.org'''
@@ -26,18 +55,21 @@ class OpenTopoData(ElevationData):
         contents = json.loads(contents)
         return contents['status'] == 'OK'
     
-    def get_elevations(self, lat_lng_list):
+    def get_elevations(self, lat_start, long_start, lat_end, long_end, nx, ny, 
+            d_mult):
+        lat_long_list = self.get_lat_long_grid(lat_start, long_start, lat_end,
+                long_end, nx, ny, d_mult)
         # public API: 1000 calls/day, 100 locations/call, 1 call/sec
         # build the request by joining the locations
         elevations = []
         dataset = 'aster30m'
         url = self.base_url + '/v1/' + dataset + '?locations='
-        for i,lat_lng in enumerate(lat_lng_list):
-            url += str(lat_lng[0]) + ',' + str(lat_lng[1])
+        for i,lat_long in enumerate(lat_long_list):
+            url += str(lat_long[0]) + ',' + str(lat_long[1])
             url += '|'
            
             # make request if we've reached 100 locations
-            if ((i+1) % 100 == 0) or (i == len(lat_lng_list)-1):
+            if ((i+1) % 100 == 0) or (i == len(lat_long_list)-1):
                 url = url[:-1]
                 # request the data
                 contents = urllib.request.urlopen(url).read()
@@ -52,15 +84,18 @@ class OpenTopoData(ElevationData):
         return elevations
 
 class EPQSData(ElevationData):
-    '''Elevation data from nationalmap.gov/epqs/'''
+    '''Elevation data from nationalmap.gov/epqs'''
     def __init__(self):
         self.base_url = 'https://nationalmap.gov/epqs/'
 
-    def get_elevations(self, lat_lng_list):
+    def get_elevations(self, lat_start, long_start, lat_end, long_end, nx, ny, 
+            d_mult):
+        lat_long_list = self.get_lat_long_grid(lat_start, long_start, lat_end,
+                long_end, nx, ny, d_mult)
         elevations = []
-        for lat_lng in lat_lng_list:
+        for lat_long in lat_long_list:
             url = self.base_url + 'pqs.php?'
-            url += 'x=' + str(lat_lng[0]) + '&y=' + str(lat_lng[1])
+            url += 'x=' + str(lat_long[0]) + '&y=' + str(lat_long[1])
             url += '&output=json&units=Meters'
         
             # request the data
@@ -74,10 +109,36 @@ class EPQSData(ElevationData):
 
         return elevations
 
+class BingMapData(ElevationData):
+    '''Elevation data from Bing Maps'''
+    def __init__(self):
+        self.base_url = 'http://dev.virtualearth.net/REST/v1/Elevation/'
+        with open('bing_maps_api_key', 'r') as file:
+            self.api_key = file.read()
+
+    def get_elevations(self, lat_start, long_start, lat_end, long_end, nx, ny, 
+            d_mult):
+        # build the request url
+        url = self.base_url + 'Bounds?bounds=' + str(lat_start) + ',' + \
+                str(long_start) + ',' + str(lat_end) + ',' + str(long_end)
+        url += '&rows=' + str(nx) + '&cols=' + str(ny) + '&key=' + self.api_key
+
+        # request the data
+        contents = urllib.request.urlopen(url).read()
+        contents = json.loads(contents)
+
+        return contents['resourceSets'][0]['resources'][0]['elevations']
+
 if __name__ == "__main__":
+    # TODO
+    bmd = BingMapData()
+    print(bmd.get_elevations(50, 50, 60, 60, 32, 32, 1.2))
+
+    '''
     otd = OpenTopoData()
     print(otd.is_healthy())
     print(otd.get_elevations([(-43.5,172.5), (27.6,1.98)]))
 
     epqs = EPQSData()
     print(epqs.get_elevations([(36.1,-115.3)]))
+    '''
